be88e1b64d00d8ac37be67b7100235c8dd1eb1cc
Larry Savidge <savidge00@gmail.com>
2020-04-22 15:26:17 -0400

    heyyo1

--- more_routes.py
+++ more_routes.py
@@ -9,6 +9,10 @@ def hello():
 @app.route("/John")
 def John():
 	return "Hello John!"
+	
+@app.route("/Welcome/<name>") # at the endpoint /<name>
+def Welcome_name(name): # call method Welcome_name
+	return "Welcome " + name + "!" # which returns "Welcome + name + !
 
 if __name__ == "__main__":
 	app.run(debug=True)
