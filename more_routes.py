d826dd0245e2690f684a6180e83daf6d4806d44e
Larry Savidge <savidge00@gmail.com>
2020-04-22 15:21:48 -0400

    heyyo

---
+++ more_routes.py
@@ -0,0 +1,14 @@
+#Using More Routes
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "Hello World!"
+
+@app.route("/John")
+def John():
+	return "Hello John!"
+
+if __name__ == "__main__":
+	app.run(debug=True)
